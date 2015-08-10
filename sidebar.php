20bb4019f2beac33c714787e15ed3b71f0beacc5
Mauricio Gamboa <magalocr@gmail.com>
2015-08-10 14:09:19 -0600

    940

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,5 @@
 <aside id="sidebar" class="sidebar">
+  <div class="bg-sidebar"></div>
   <?php get_template_part('template_parts/content', 'widget-most-viewed'); ?>
   <?php get_template_part('template_parts/content', 'widget-categories'); ?>
   <?php get_template_part('template_parts/content', 'widget-mailchimp'); ?>
