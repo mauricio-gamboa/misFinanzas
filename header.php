b69322921f65de9bcf8dc564345d117325018c12
Mauricio Gamboa <magalocr@gmail.com>
2015-09-03 16:04:54 -0600

    Favicon

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+  <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/public/images/favicon.ico" />
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
